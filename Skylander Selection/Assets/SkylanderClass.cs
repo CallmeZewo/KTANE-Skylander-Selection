acc5049f40c56c9dfe8e71bec66897b33fc3fe84
Zero <120384602+CallmeZewo@users.noreply.github.com>
2026-01-10 17:11:18 +0100

    Added SkylanderClass script

---
+++ Skylander Selection/Assets/SkylanderClass.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using static Enums;
+
+public class SkylanderClass
+{
+    public Sprite Sprite;
+    public ElementEnum Element;
+    public AudioClip Catchphrase;
+
+    public SkylanderClass(Sprite sprite, ElementEnum elementEnum, AudioClip catchphrase)
+    {
+        Sprite = sprite;
+        Element = elementEnum;
+        Catchphrase = catchphrase;
+    }
+}
